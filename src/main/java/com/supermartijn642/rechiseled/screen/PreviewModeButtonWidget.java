766efcf2701b78d47841e320ad7a331fec497ef3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-01-05 01:50:34 +0100

    Move PreviewModeButtonWidget to the screen package

--- src/main/java/com/supermartijn642/rechiseled/screen/PreviewModeButtonWidget.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/PreviewModeButtonWidget.java
@@ -1,4 +1,4 @@
-package com.supermartijn642.rechiseled.model;
+package com.supermartijn642.rechiseled.screen;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.supermartijn642.core.TextComponents;
