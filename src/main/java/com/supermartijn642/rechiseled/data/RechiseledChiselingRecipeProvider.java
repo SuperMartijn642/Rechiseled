56fb1625219b5dae9baf82b8e0d5ddaadb49649b
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-01-07 22:00:38 +0100

    Add new variants for dark prismarine, nether bricks, prismarine bricks, and red nether bricks

--- src/main/java/com/supermartijn642/rechiseled/data/RechiseledChiselingRecipeProvider.java
+++ src/main/java/com/supermartijn642/rechiseled/data/RechiseledChiselingRecipeProvider.java
@@ -47,6 +47,8 @@ public class RechiseledChiselingRecipeProvider extends ChiselingRecipeProvider {
             .addRegularItem(Items.CRIMSON_PLANKS);
         builders.computeIfAbsent(RechiseledTagGroups.DARK_OAK_PLANKS, this::beginRecipe)
             .addRegularItem(Items.DARK_OAK_PLANKS);
+        builders.computeIfAbsent(RechiseledTagGroups.DARK_PRISMARINE, this::beginRecipe)
+            .addRegularItem(Items.DARK_PRISMARINE);
         builders.computeIfAbsent(RechiseledTagGroups.DIORITE, this::beginRecipe)
             .addRegularItem(Items.DIORITE);
         builders.computeIfAbsent(RechiseledTagGroups.END_STONE, this::beginRecipe)
