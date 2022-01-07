b3f10b811c080b7cc7d46f38f1556073fd8adf12
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-01-07 22:27:18 +0100

    update version

--- src/main/java/com/supermartijn642/rechiseled/Rechiseled.java
+++ src/main/java/com/supermartijn642/rechiseled/Rechiseled.java
@@ -29,7 +29,7 @@ public class Rechiseled {
 
     public static final String MODID = "rechiseled";
     public static final String NAME = "Rechiseled";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     public static final String DEPENDENCIES = "required-after:supermartijn642corelib@[1.0.15,);required-after:supermartijn642configlib@[1.0.9,)";
 
     public static final PacketChannel CHANNEL = PacketChannel.create("rechiseled");
