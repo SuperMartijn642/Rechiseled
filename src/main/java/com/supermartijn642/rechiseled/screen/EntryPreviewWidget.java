1ec7a03d289ca69eb560398fdf7a7e6c6e7d8f58
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-01-22 23:50:44 +0100

    Properly render items which aren't BlockItems in the chiseling screen

--- src/main/java/com/supermartijn642/rechiseled/screen/EntryPreviewWidget.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/EntryPreviewWidget.java
@@ -3,6 +3,8 @@ package com.supermartijn642.rechiseled.screen;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.supermartijn642.core.gui.widget.Widget;
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 
@@ -11,11 +13,11 @@ import java.util.function.Supplier;
 /**
  * Created 25/12/2021 by SuperMartijn642
  */
-public class BlockPreviewWidget extends Widget {
+public class EntryPreviewWidget extends Widget {
 
     private static final int ROTATION_TIME = 10000;
 
-    private final Supplier<Block> block;
+    private final Supplier<Item> item;
     private final Supplier<Integer> previewMode;
     private final Supplier<Integer> guiLeft, guiTop;
 
@@ -24,13 +26,13 @@ public class BlockPreviewWidget extends Widget {
     private boolean dragging = false;
     private int mouseStartX, mouseStartY;
 
-    public BlockPreviewWidget(int x, int y, int width, int height,
-                              Supplier<Block> block,
+    public EntryPreviewWidget(int x, int y, int width, int height,
+                              Supplier<Item> item,
                               Supplier<Integer> previewMode,
                               Supplier<Integer> guiLeft,
                               Supplier<Integer> guiTop){
         super(x, y, width, height);
-        this.block = block;
+        this.item = item;
         this.previewMode = previewMode;
         this.guiLeft = guiLeft;
         this.guiTop = guiTop;
@@ -46,9 +48,10 @@ public class BlockPreviewWidget extends Widget {
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks){
         long now = System.currentTimeMillis();
 
-        Block block = this.block.get();
+        Item item = this.item.get();
         int previewMode = this.previewMode.get();
-        if(block != null && previewMode >= 0 && previewMode <= 2){
+        if(item != null && previewMode >= 0 && previewMode <= 2){
+            // Update the rotation
             if(this.dragging){
                 this.yaw += (mouseX - this.mouseStartX) / 100d * 360;
                 this.pitch += (mouseY - this.mouseStartY) / 100d * 360;
@@ -57,19 +60,27 @@ public class BlockPreviewWidget extends Widget {
             }else
                 this.yaw += (double)(now - this.lastRotationTime) / ROTATION_TIME * 360;
 
-            BlockCapture capture;
-            if(previewMode == 0)
-                capture = new BlockCapture(block);
-            else if(previewMode == 1){
-                capture = new BlockCapture(block);
-                capture.putBlock(new BlockPos(-1, 0, 0), block);
-                capture.putBlock(new BlockPos(1, 0, 0), block);
+            // Render the item or block
+            if(item instanceof BlockItem){
+                // Render block
+                Block block = ((BlockItem)item).getBlock();
+                BlockCapture capture;
+                if(previewMode == 0)
+                    capture = new BlockCapture(block);
+                else if(previewMode == 1){
+                    capture = new BlockCapture(block);
+                    capture.putBlock(new BlockPos(-1, 0, 0), block);
+                    capture.putBlock(new BlockPos(1, 0, 0), block);
+                }else{
+                    capture = new BlockCapture();
+                    for(int i = 0; i < 9; i++)
+                        capture.putBlock(new BlockPos(i / 3 - 1, i % 3 - 1, 0), block);
+                }
+                ScreenBlockRenderer.drawBlock(capture, this.guiLeft.get() + this.x + this.width / 2d, this.guiTop.get() + this.y + this.height / 2d, this.width, this.yaw, this.pitch, false);
             }else{
-                capture = new BlockCapture();
-                for(int i = 0; i < 9; i++)
-                    capture.putBlock(new BlockPos(i / 3 - 1, i % 3 - 1, 0), block);
+                // Render item
+                ScreenItemRender.drawItem(item, this.guiLeft.get() + this.x + this.width / 2d, this.guiTop.get() + this.y + this.height / 2d, this.width, this.yaw, this.pitch, true);
             }
-            ScreenBlockRenderer.drawBlock(capture, this.guiLeft.get() + this.x + this.width / 2d, this.guiTop.get() + this.y + this.height / 2d, this.width, this.yaw, this.pitch, false);
         }
 
         this.lastRotationTime = now;
---
+++ src/main/java/com/supermartijn642/rechiseled/screen/ScreenItemRender.java
@@ -0,0 +1,58 @@
+package com.supermartijn642.rechiseled.screen;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.supermartijn642.core.ClientUtils;
+import com.supermartijn642.core.render.RenderUtils;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.vector.Quaternion;
+
+/**
+ * Created 22/01/2022 by SuperMartijn642
+ */
+public class ScreenItemRender {
+
+    /**
+     * Renders a given item as a 3d model
+     */
+    public static void drawItem(Item item, double x, double y, double scale, float yaw, float pitch, boolean doShading){
+        scale /= Math.sqrt(2 + 1d / (16 * 16));
+
+        RenderSystem.enableRescaleNormal();
+        RenderSystem.enableAlphaTest();
+        RenderSystem.defaultAlphaFunc();
+        RenderSystem.enableBlend();
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+
+        RenderSystem.pushMatrix();
+        RenderSystem.translated(x, y, 350);
+        RenderSystem.scalef(1, -1, 1);
+        RenderSystem.scaled(scale, scale, scale);
+
+        MatrixStack matrixstack = new MatrixStack();
+        matrixstack.mulPose(new Quaternion(pitch, yaw, 0, true));
+
+        if(doShading)
+            RenderSystem.enableLighting();
+
+        IRenderTypeBuffer.Impl renderTypeBuffer = RenderUtils.getMainBufferSource();
+        IBakedModel model = ClientUtils.getItemRenderer().getItemModelShaper().getItemModel(item);
+        if(model != null)
+            ClientUtils.getItemRenderer().render(item.getDefaultInstance(), ItemCameraTransforms.TransformType.GUI, false, matrixstack, renderTypeBuffer, 15728880, OverlayTexture.NO_OVERLAY, model);
+        renderTypeBuffer.endBatch();
+
+        RenderSystem.enableDepthTest();
+        if(doShading)
+            RenderSystem.disableLighting();
+
+        RenderSystem.popMatrix();
+
+        RenderSystem.disableAlphaTest();
+        RenderSystem.disableRescaleNormal();
+    }
+}
