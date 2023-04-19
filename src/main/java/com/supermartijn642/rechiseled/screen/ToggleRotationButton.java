3ef6c81874c9dad7da61180a45fb081fea391167
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-04-20 01:53:01 +0200

    Add a button to toggle whether the preview rotates

---
+++ src/main/java/com/supermartijn642/rechiseled/screen/ToggleRotationButton.java
@@ -0,0 +1,33 @@
+package com.supermartijn642.rechiseled.screen;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.supermartijn642.core.gui.ScreenUtils;
+import com.supermartijn642.core.gui.widget.premade.AbstractButtonWidget;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+/**
+ * Created 20/04/2023 by SuperMartijn642
+ */
+public class ToggleRotationButton extends AbstractButtonWidget {
+
+    private static final ResourceLocation TEXTURE = new ResourceLocation("rechiseled", "textures/screen/rotation_icon.png");
+
+    public static boolean rotate = true;
+
+    public ToggleRotationButton(int x, int y, int width, int height){
+        super(x, y, width, height, () -> rotate = !rotate);
+    }
+
+    @Override
+    public void render(PoseStack poseStack, int mouseX, int mouseY){
+        ScreenUtils.bindTexture(TEXTURE);
+        ScreenUtils.drawTexture(poseStack, this.x, this.y, this.width, this.height, rotate ? 0 : 0.5f, this.isFocused() ? 0.5f : 0, 0.5f, 0.5f);
+        super.render(poseStack, mouseX, mouseY);
+    }
+
+    @Override
+    public Component getNarrationMessage(){
+        return null;
+    }
+}
