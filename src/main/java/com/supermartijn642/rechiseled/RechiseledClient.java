60ee8fc6135340c92da3f215e965fc241a41b46f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-06-18 01:30:54 +0200

    Render the item stored in the chisel

--- src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
+++ src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
@@ -3,6 +3,7 @@ package com.supermartijn642.rechiseled;
 import com.supermartijn642.core.TextComponents;
 import com.supermartijn642.core.gui.WidgetContainerScreen;
 import com.supermartijn642.core.registry.ClientRegistrationHandler;
+import com.supermartijn642.core.render.CustomRendererBakedModelWrapper;
 import com.supermartijn642.rechiseled.screen.BaseChiselingContainerScreen;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.relauncher.Side;
@@ -16,5 +17,7 @@ public class RechiseledClient {
     public static void register(){
         ClientRegistrationHandler handler = ClientRegistrationHandler.get("rechiseled");
         handler.registerContainerScreen(() -> Rechiseled.chisel_container, container -> new WidgetContainerScreen<>(new BaseChiselingContainerScreen<>(TextComponents.item(Rechiseled.chisel).get()), container, false));
+        handler.registerItemModelOverwrite(() -> Rechiseled.chisel, CustomRendererBakedModelWrapper::wrap);
+        handler.registerCustomItemRenderer(() -> Rechiseled.chisel, ChiselItemRenderer::new);
     }
 }
