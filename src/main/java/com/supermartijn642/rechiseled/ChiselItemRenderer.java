3119fcf15690f58dbbffd4412cfc069f1dfc4a78
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-06-19 19:42:17 +0200

    Fix ChiselItemRenderer

--- src/main/java/com/supermartijn642/rechiseled/ChiselItemRenderer.java
+++ src/main/java/com/supermartijn642/rechiseled/ChiselItemRenderer.java
@@ -6,9 +6,9 @@ import com.mojang.math.MatrixUtil;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.render.CustomItemRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
 /**
@@ -17,21 +17,21 @@ import net.minecraft.world.item.ItemStack;
 public class ChiselItemRenderer implements CustomItemRenderer {
 
     @Override
-    public void render(ItemStack stack, ItemDisplayContext transformType, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLight, int combinedOverlay){
+    public void render(ItemStack stack, ItemTransforms.TransformType transformType, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLight, int combinedOverlay){
         renderChisel(stack, transformType, poseStack, bufferSource, combinedLight, combinedOverlay);
-        if(transformType == ItemDisplayContext.GUI){
+        if(transformType == ItemTransforms.TransformType.GUI){
             ItemStack storedStack = ChiselItem.getStoredStack(stack);
             if(!storedStack.isEmpty()){
                 poseStack.pushPose();
                 poseStack.translate(0.25, 0.75, 1);
                 poseStack.scale(0.5f, 0.5f, 0.5f);
-                ClientUtils.getItemRenderer().renderStatic(storedStack, ItemDisplayContext.GUI, combinedLight, combinedOverlay, poseStack, bufferSource, null, 0);
+                ClientUtils.getItemRenderer().renderStatic(storedStack, ItemTransforms.TransformType.GUI, combinedLight, combinedOverlay, poseStack, bufferSource, 0);
                 poseStack.popPose();
             }
         }
     }
 
-    private static void renderChisel(ItemStack stack, ItemDisplayContext transformType, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLight, int combinedOverlay){
+    private static void renderChisel(ItemStack stack, ItemTransforms.TransformType transformType, PoseStack poseStack, MultiBufferSource bufferSource, int combinedLight, int combinedOverlay){
         ItemRenderer renderer = ClientUtils.getItemRenderer();
         BakedModel model = renderer.getModel(stack, null, null, 0);
         for(var subModel : model.getRenderPasses(stack, true)){
@@ -40,7 +40,7 @@ public class ChiselItemRenderer implements CustomItemRenderer {
                 if(stack.hasFoil()){
                     poseStack.pushPose();
                     PoseStack.Pose pose = poseStack.last();
-                    if(transformType == ItemDisplayContext.GUI)
+                    if(transformType == ItemTransforms.TransformType.GUI)
                         MatrixUtil.mulComponentWise(pose.pose(), 0.5f);
                     else if(transformType.firstPerson())
                         MatrixUtil.mulComponentWise(pose.pose(), 0.75f);
