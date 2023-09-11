162a5065bf16bda3d2a94606791508e82c152816
SuperMartijn624 <martijnvandenbrand@outlook.com>
2023-09-11 12:31:24 +0200

    Add variants for blue ice, bone block, iron block, lapis block, netherite block, and redstone block

--- src/main/java/com/supermartijn642/rechiseled/api/BaseChiselingRecipes.java
+++ src/main/java/com/supermartijn642/rechiseled/api/BaseChiselingRecipes.java
@@ -15,6 +15,8 @@ public class BaseChiselingRecipes {
     public static final ResourceLocation BASALT = location("basalt");
     public static final ResourceLocation BIRCH_PLANKS = location("birch_planks");
     public static final ResourceLocation BLACKSTONE = location("blackstone");
+    public static final ResourceLocation BLUE_ICE = location("blue_ice");
+    public static final ResourceLocation BONE_BLOCK = location("bone_block");
     public static final ResourceLocation COAL_BLOCK = location("coal_block");
     public static final ResourceLocation COBBLED_DEEPSLATE = location("cobbled_deepslate");
     public static final ResourceLocation COBBLESTONE = location("cobblestone");
@@ -30,9 +32,12 @@ public class BaseChiselingRecipes {
     public static final ResourceLocation GLOWSTONE = location("glowstone");
     public static final ResourceLocation GOLD_BLOCK = location("gold_block");
     public static final ResourceLocation GRANITE = location("granite");
+    public static final ResourceLocation IRON_BLOCK = location("iron_block");
     public static final ResourceLocation JUNGLE_PLANKS = location("jungle_planks");
+    public static final ResourceLocation LAPIS_BLOCK = location("lapis_block");
     public static final ResourceLocation NETHERRACK = location("netherrack");
     public static final ResourceLocation NETHER_BRICKS = location("nether_bricks");
+    public static final ResourceLocation NETHERITE_BLOCK = location("netherite_block");
     public static final ResourceLocation OAK_PLANKS = location("oak_planks");
     public static final ResourceLocation OBSIDIAN = location("obsidian");
     public static final ResourceLocation PRISMARINE_BRICKS = location("prismarine_bricks");
@@ -40,6 +45,7 @@ public class BaseChiselingRecipes {
     public static final ResourceLocation QUARTZ_BLOCK = location("quartz_block");
     public static final ResourceLocation RED_NETHER_BRICKS = location("red_nether_bricks");
     public static final ResourceLocation RED_SANDSTONE = location("red_sandstone");
+    public static final ResourceLocation REDSTONE_BLOCK = location("redstone_block");
     public static final ResourceLocation SANDSTONE = location("sandstone");
     public static final ResourceLocation SPRUCE_PLANKS = location("spruce_planks");
     public static final ResourceLocation STONE = location("stone");
