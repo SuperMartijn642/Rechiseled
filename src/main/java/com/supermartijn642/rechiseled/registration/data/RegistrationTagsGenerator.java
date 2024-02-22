dd4c408b85170be564221709f4b146f3d04bc47c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-02-22 23:51:47 +0100

    Check all datapacks rather than just vanilla ones in `RegistrationTagsGenerator`

--- src/main/java/com/supermartijn642/rechiseled/registration/data/RegistrationTagsGenerator.java
+++ src/main/java/com/supermartijn642/rechiseled/registration/data/RegistrationTagsGenerator.java
@@ -5,19 +5,31 @@ import com.google.gson.*;
 import com.supermartijn642.core.generator.ResourceCache;
 import com.supermartijn642.core.generator.TagGenerator;
 import com.supermartijn642.core.registry.Registries;
+import com.supermartijn642.rechiseled.Rechiseled;
 import com.supermartijn642.rechiseled.blocks.RechiseledBlockBuilderImpl;
 import com.supermartijn642.rechiseled.blocks.RechiseledBlockTypeImpl;
 import com.supermartijn642.rechiseled.registration.RechiseledRegistrationImpl;
+import net.fabricmc.fabric.api.resource.ModResourcePack;
+import net.fabricmc.fabric.impl.resource.loader.FabricModResourcePack;
+import net.fabricmc.fabric.impl.resource.loader.GroupResourcePack;
+import net.fabricmc.fabric.impl.resource.loader.ModResourcePackCreator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.VanillaPackResources;
+import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.ServerPacksSource;
+import net.minecraft.server.packs.resources.MultiPackResourceManager;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -32,12 +44,54 @@ import java.util.stream.Stream;
 public class RegistrationTagsGenerator extends TagGenerator {
 
     private static final Gson GSON = new GsonBuilder().create();
+    private static final List<PackResources> ALL_DATA_PACKS;
+
+    static{
+        Field packsField;
+        try{
+            packsField = GroupResourcePack.class.getDeclaredField("packs");
+            packsField.setAccessible(true);
+        }catch(NoSuchFieldException e){
+            throw new RuntimeException(e);
+        }
+
+        List<PackResources> packs = new ArrayList<>();
+        packs.add(new VanillaPackResources(ServerPacksSource.BUILT_IN_METADATA, ServerPacksSource.VANILLA_ID));
+        new ModResourcePackCreator(PackType.SERVER_DATA).loadPacks(pack -> {
+            try{
+                packs.add(pack.open());
+            }catch(Exception e){
+                Rechiseled.LOGGER.info("Encountered an exception whilst loading data packs for 'RegistrationTagsGenerator'!", e);
+            }
+        }, (name, title, required, resources, metadata, position, source) ->
+            new Pack(name, title, required, resources, metadata, PackType.SERVER_DATA, position, source));
+        ALL_DATA_PACKS = packs.stream()
+            .flatMap(pack -> {
+                if(pack instanceof FabricModResourcePack){
+                    try{
+                        //noinspection unchecked
+                        return ((List<? extends PackResources>)packsField.get(pack)).stream();
+                    }catch(IllegalAccessException e){
+                        throw new RuntimeException(e);
+                    }
+                }
+                return Stream.of(pack);
+            })
+            .toList();
+    }
 
     private final RechiseledRegistrationImpl registration;
+    private final ResourceManager resources;
 
     public RegistrationTagsGenerator(RechiseledRegistrationImpl registration, ResourceCache cache){
         super(registration.getModid(), cache);
         this.registration = registration;
+        this.resources = new MultiPackResourceManager(
+            PackType.SERVER_DATA,
+            ALL_DATA_PACKS.stream()
+                .filter(pack -> !(pack instanceof ModResourcePack) || !((ModResourcePack)pack).getFabricModMetadata().getId().equals(registration.getModid()))
+                .toList()
+        );
     }
 
     @Override
@@ -86,28 +140,37 @@ public class RegistrationTagsGenerator extends TagGenerator {
         List<Block> blocks = new ArrayList<>();
 
         ResourceLocation tagLocation = new ResourceLocation(location.getNamespace(), "tags/blocks/" + location.getPath() + ".json");
-        try(VanillaPackResources resources = new VanillaPackResources(ServerPacksSource.BUILT_IN_METADATA, ServerPacksSource.VANILLA_ID)){ // Properly load tags
-            try(InputStream stream = resources.getResource(PackType.SERVER_DATA, tagLocation)){
-                JsonObject json = GSON.fromJson(new InputStreamReader(stream), JsonObject.class);
-                JsonArray array = json.getAsJsonArray("values");
-                for(JsonElement element : array){
-                    String name = element.getAsString();
-                    if(name.charAt(0) == '#'){
-                        blocks.addAll(this.loadVanillaTag(new ResourceLocation(name.substring(1))));
-                        continue;
+        try{
+            for(Resource resource : this.resources.getResources(tagLocation)){
+                try(InputStream stream = resource.getInputStream()){
+                    JsonObject json = GSON.fromJson(new InputStreamReader(stream), JsonObject.class);
+                    JsonArray array = json.getAsJsonArray("values");
+                    for(JsonElement element : array){
+                        String name = element.getAsString();
+                        if(name.charAt(0) == '#'){
+                            blocks.addAll(this.loadVanillaTag(new ResourceLocation(name.substring(1))));
+                            continue;
+                        }
+                        ResourceLocation registryName = new ResourceLocation(name);
+                        Block block = Registries.BLOCKS.getValue(registryName);
+                        if(block == null)
+                            throw new JsonParseException("Unknown block '" + registryName + "' in '" + location + "'");
+                        blocks.add(block);
                     }
-                    ResourceLocation registryName = new ResourceLocation(name);
-                    Block block = Registries.BLOCKS.getValue(registryName);
-                    if(block == null)
-                        throw new JsonParseException("Unknown block '" + registryName + "' in '" + location + "'");
-                    blocks.add(block);
+                }catch(Exception e){
+                    e.printStackTrace();
                 }
-            }catch(Exception e){
-                e.printStackTrace();
             }
+        }catch(IOException e){
+            throw new RuntimeException("Encountered an exception whilst iteration resources for tag '" + tagLocation + "':", e);
         }
 
         this.loadedTags.put(location, blocks);
         return blocks;
+    }
+
+    @Override
+    public String getName(){
+        return "Registration Tag Generator: " + this.modName;
     }
 }
