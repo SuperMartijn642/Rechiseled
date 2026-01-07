4e0c849fff7b9707c96c3af61439cca4aef0ce4f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-07 17:53:22 +0100

    Replace 'rechiseled' strings with MODID field

--- src/main/java/com/supermartijn642/rechiseled/Rechiseled.java
+++ src/main/java/com/supermartijn642/rechiseled/Rechiseled.java
@@ -15,6 +15,7 @@ import com.supermartijn642.rechiseled.packet.PacketChiselAll;
 import com.supermartijn642.rechiseled.packet.PacketSelectEntry;
 import com.supermartijn642.rechiseled.packet.PacketToggleConnecting;
 import com.supermartijn642.rechiseled.screen.ChiselContainer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -30,20 +31,26 @@ import java.util.Objects;
 /**
  * Created 7/7/2020 by SuperMartijn642
  */
-@Mod("rechiseled")
+@Mod(Rechiseled.MODID)
 public class Rechiseled {
 
-    public static final RechiseledRegistration REGISTRATION = RechiseledRegistration.get("rechiseled");
-    public static final PacketChannel CHANNEL = PacketChannel.create("rechiseled");
-    public static final Logger LOGGER = LoggerFactory.getLogger("rechiseled");
+    public static final String MODID = "rechiseled";
 
-    @RegistryEntryAcceptor(namespace = "rechiseled", identifier = "chisel", registry = RegistryEntryAcceptor.Registry.ITEMS)
+    public static ResourceLocation identifier(String path){
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
+    }
+
+    public static final RechiseledRegistration REGISTRATION = RechiseledRegistration.get(MODID);
+    public static final PacketChannel CHANNEL = PacketChannel.create(MODID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+    @RegistryEntryAcceptor(namespace = MODID, identifier = "chisel", registry = RegistryEntryAcceptor.Registry.ITEMS)
     public static ChiselItem chisel;
 
-    @RegistryEntryAcceptor(namespace = "rechiseled", identifier = "chisel_container", registry = RegistryEntryAcceptor.Registry.MENU_TYPES)
+    @RegistryEntryAcceptor(namespace = MODID, identifier = "chisel_container", registry = RegistryEntryAcceptor.Registry.MENU_TYPES)
     public static BaseContainerType<ChiselContainer> chisel_container;
 
-    public static final CreativeItemGroup GROUP = CreativeItemGroup.create("rechiseled", () -> chisel)
+    public static final CreativeItemGroup GROUP = CreativeItemGroup.create(MODID, () -> chisel)
         .filler(stackConsumer -> {
             List<Item> items = new LinkedList<>();
             items.add(chisel);
@@ -69,7 +76,7 @@ public class Rechiseled {
     }
 
     public static void register(){
-        RegistrationHandler handler = RegistrationHandler.get("rechiseled");
+        RegistrationHandler handler = RegistrationHandler.get(MODID);
         // Chisel item
         handler.registerItem("chisel", ChiselItem::new);
         // Container type
@@ -79,7 +86,7 @@ public class Rechiseled {
     }
 
     private static void registerGenerators(){
-        GeneratorRegistrationHandler handler = GeneratorRegistrationHandler.get("rechiseled");
+        GeneratorRegistrationHandler handler = GeneratorRegistrationHandler.get(MODID);
         handler.addProvider(RechiseledTextureProvider::new);
         //noinspection Convert2MethodRef
         handler.addGenerator(cache -> new RechiseledItemInfoGenerator(cache));
