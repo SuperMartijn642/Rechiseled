313366338da590717351b02d10fd8563791765ab
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-07 17:53:22 +0100

    Replace 'rechiseled' strings with MODID field

--- src/main/java/com/supermartijn642/rechiseled/api/BaseChiselingRecipes.java
+++ src/main/java/com/supermartijn642/rechiseled/api/BaseChiselingRecipes.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.rechiseled.api;
 
+import com.supermartijn642.rechiseled.Rechiseled;
 import net.minecraft.resources.ResourceLocation;
 
 /**
@@ -56,6 +57,6 @@ public class BaseChiselingRecipes {
     public static final ResourceLocation WARPED_PLANKS = location("warped_planks");
 
     private static ResourceLocation location(String name){
-        return ResourceLocation.fromNamespaceAndPath("rechiseled", name);
+        return Rechiseled.identifier(name);
     }
 }
