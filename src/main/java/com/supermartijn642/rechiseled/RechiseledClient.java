76435bfea68c6412fa9bdc9ab88536f475c3feea
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-07 17:53:22 +0100

    Replace 'rechiseled' strings with MODID field

--- src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
+++ src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
@@ -12,7 +12,7 @@ import com.supermartijn642.rechiseled.screen.BaseChiselingContainerScreen;
 public class RechiseledClient {
 
     public static void register(){
-        ClientRegistrationHandler handler = ClientRegistrationHandler.get("rechiseled");
+        ClientRegistrationHandler handler = ClientRegistrationHandler.get(Rechiseled.MODID);
         handler.registerContainerScreen(() -> Rechiseled.chisel_container, container -> WidgetContainerScreen.of(new BaseChiselingContainerScreen<>(TextComponents.item(Rechiseled.chisel).get()), container, false));
         handler.registerItemModelOverwrite(() -> Rechiseled.chisel, CustomRendererBakedModelWrapper::wrap);
         handler.registerCustomItemRenderer(() -> Rechiseled.chisel, ChiselItemRenderer::new);
