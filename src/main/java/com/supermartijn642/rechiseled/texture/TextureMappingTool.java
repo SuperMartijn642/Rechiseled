1118baa35632bc78ac409509636b8326e87eca28
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-07 17:53:22 +0100

    Replace 'rechiseled' strings with MODID field

--- src/main/java/com/supermartijn642/rechiseled/texture/TextureMappingTool.java
+++ src/main/java/com/supermartijn642/rechiseled/texture/TextureMappingTool.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.rechiseled.texture;
 
+import com.supermartijn642.rechiseled.Rechiseled;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.resources.MultiPackResourceManager;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -8,8 +9,8 @@ import net.minecraftforge.common.data.ExistingFileHelper;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.*;
+import java.util.List;
 
 /**
  * Created 24/01/2022 by SuperMartijn642
@@ -73,10 +74,10 @@ public class TextureMappingTool {
 
         List<String> suffixes = new ArrayList<>();
         resourceManager.listPacks().filter(
-            pack -> pack.getNamespaces(PackType.CLIENT_RESOURCES).size() == 1 && pack.getNamespaces(PackType.CLIENT_RESOURCES).contains("rechiseled")
+            pack -> pack.getNamespaces(PackType.CLIENT_RESOURCES).size() == 1 && pack.getNamespaces(PackType.CLIENT_RESOURCES).contains(Rechiseled.MODID)
         ).forEach(
             pack -> {
-                pack.getResources(PackType.CLIENT_RESOURCES, "rechiseled", "textures/block", Integer.MAX_VALUE, s -> s.startsWith(name) && s.endsWith(".png"))
+                pack.getResources(PackType.CLIENT_RESOURCES, Rechiseled.MODID, "textures/block", Integer.MAX_VALUE, s -> s.startsWith(name) && s.endsWith(".png"))
                     .stream()
                     .map(
                         s -> {
