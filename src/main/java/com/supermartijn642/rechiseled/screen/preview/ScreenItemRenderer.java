0b512effb99203f09b5479eaf001faef371fe97d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:11:47 +0100

    Rename `ScreenItemRender` to `ScreenItemRenderer`

--- src/main/java/com/supermartijn642/rechiseled/screen/preview/ScreenItemRenderer.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/preview/ScreenItemRenderer.java
@@ -11,7 +11,7 @@ import net.minecraft.world.item.ItemDisplayContext;
 /**
  * Created 22/01/2022 by SuperMartijn642
  */
-public class ScreenItemRender {
+public class ScreenItemRenderer {
 
     /**
      * Renders a given item as a 3d model
