8c1591e816f822eba62e09b76bc1303f9c6d0174
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:11:47 +0100

    Rename `ScreenItemRender` to `ScreenItemRenderer`

--- src/main/java/com/supermartijn642/rechiseled/screen/EntryButtonWidget.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/EntryButtonWidget.java
@@ -5,7 +5,7 @@ import com.supermartijn642.core.TextComponents;
 import com.supermartijn642.core.gui.ScreenUtils;
 import com.supermartijn642.core.gui.widget.premade.AbstractButtonWidget;
 import com.supermartijn642.rechiseled.Rechiseled;
-import com.supermartijn642.rechiseled.screen.preview.ScreenItemRender;
+import com.supermartijn642.rechiseled.screen.preview.ScreenItemRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -79,7 +79,7 @@ public class EntryButtonWidget extends AbstractButtonWidget {
         DisplayEntry display = this.entry.get();
         if(display != null){
             Item item = display.getItem(this.connecting.get());
-            ScreenItemRender.drawItem(item, this.x + this.width / 2d, this.y + this.height / 2d, (this.width - 2) * 1.416, 0, 0, false);
+            ScreenItemRenderer.drawItem(item, this.x + this.width / 2d, this.y + this.height / 2d, (this.width - 2) * 1.416, 0, 0, false);
         }
     }
 }
