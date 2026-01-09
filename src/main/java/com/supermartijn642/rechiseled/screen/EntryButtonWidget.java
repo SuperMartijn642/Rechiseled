ff402dedfebdee2fd02b58f5adffdfd056c768d1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:08:03 +0100

    Rework chisel screen with search, filters, scrolling, slabs & stairs support

--- src/main/java/com/supermartijn642/rechiseled/screen/EntryButtonWidget.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/EntryButtonWidget.java
@@ -3,80 +3,83 @@ package com.supermartijn642.rechiseled.screen;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.supermartijn642.core.TextComponents;
 import com.supermartijn642.core.gui.ScreenUtils;
-import com.supermartijn642.core.gui.widget.BaseWidget;
+import com.supermartijn642.core.gui.widget.premade.AbstractButtonWidget;
 import com.supermartijn642.rechiseled.Rechiseled;
-import com.supermartijn642.rechiseled.api.chiseling.ChiselingBlockShape;
-import com.supermartijn642.rechiseled.api.chiseling.ChiselingEntry;
+import com.supermartijn642.rechiseled.screen.preview.ScreenItemRender;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 /**
  * Created 2/3/2021 by SuperMartijn642
  */
-public class EntryButtonWidget extends BaseWidget {
+public class EntryButtonWidget extends AbstractButtonWidget {
 
     private static final ResourceLocation TEXTURE = Rechiseled.identifier("textures/screen/buttons.png");
 
-    private final Supplier<ChiselingEntry> entry;
-    private final Supplier<ChiselingEntry> selectedEntry;
-    private final Runnable onClick;
+    private final int anchorY;
+    private final Supplier<DisplayEntry> entry;
+    private final Supplier<DisplayEntry> selectedEntry;
     private final Supplier<Boolean> connecting;
 
     public EntryButtonWidget(int x, int y, int width, int height,
-                             Supplier<ChiselingEntry> entrySupplier,
-                             Supplier<ChiselingEntry> selectedEntrySupplier,
+                             Supplier<DisplayEntry> entrySupplier,
+                             Supplier<DisplayEntry> selectedEntrySupplier,
                              Runnable onClick,
                              Supplier<Boolean> connecting){
-        super(x, y, width, height);
+        super(x, y, width, height, onClick);
+        this.anchorY = y;
         this.entry = entrySupplier;
         this.selectedEntry = selectedEntrySupplier;
-        this.onClick = onClick;
         this.connecting = connecting;
+    }
+
+    public void setVerticalOffset(float offset){
+        this.y = (int)(this.anchorY - offset * this.height);
+    }
+
+    @Override
+    protected boolean isClickable(){
+        return this.entry.get() != null && this.entry.get() != this.selectedEntry.get();
     }
 
     @Override
     public Component getNarrationMessage(){
-        ChiselingEntry entry = this.entry.get();
-        if(entry == null)
+        DisplayEntry display = this.entry.get();
+        if(display == null)
             return null;
-        Item item = (this.connecting.get() && entry.hasConnectingItem(ChiselingBlockShape.BLOCK)) || !entry.hasRegularItem(ChiselingBlockShape.BLOCK) ? entry.getConnectingItem(ChiselingBlockShape.BLOCK) : entry.getRegularItem(ChiselingBlockShape.BLOCK);
-        //noinspection DataFlowIssue
+        Item item = display.getItem(this.connecting.get());
         return TextComponents.translation("rechiseled.chiseling.select_block", TextComponents.item(item).get()).get();
     }
 
     @Override
-    public void render(PoseStack poseStack, int mouseX, int mouseY){
-        ChiselingEntry entry = this.entry.get();
-
-        boolean hasEntry = entry != null;
-        boolean selected = hasEntry && this.selectedEntry.get() == entry;
-        boolean hasCorrectItem = hasEntry && (this.connecting.get() ? entry.hasConnectingItem(ChiselingBlockShape.BLOCK) : entry.hasRegularItem(ChiselingBlockShape.BLOCK));
-
-        ScreenUtils.bindTexture(TEXTURE);
-        ScreenUtils.drawTexture(poseStack, this.x, this.y, this.width, this.height, 0, (selected ? 1 : hasEntry ? hasCorrectItem ? this.isFocused() ? 2 : 0 : this.isFocused() ? 4 : 3 : 0) / 5f, 1, 1 / 5f);
-
-        if(hasEntry){
-            Item item = (this.connecting.get() && entry.hasConnectingItem(ChiselingBlockShape.BLOCK)) || !entry.hasRegularItem(ChiselingBlockShape.BLOCK) ? entry.getConnectingItem(ChiselingBlockShape.BLOCK) : entry.getRegularItem(ChiselingBlockShape.BLOCK);
-            if(item instanceof BlockItem){
-                BlockCapture capture = new BlockCapture(((BlockItem)item).getBlock());
-                ScreenBlockRenderer.drawBlock(capture, this.x + this.width / 2d, this.y + this.height / 2d, this.width, 135, 40, true);
-            }else
-                ScreenItemRender.drawItem(item, this.x + this.width / 2d, this.y + this.height / 2d, (this.width - 4) * 1.416, 0, 0, false);
-        }
+    protected void getTooltips(Consumer<Component> tooltips){
+        DisplayEntry display = this.entry.get();
+        if(display != null)
+            tooltips.accept(TextComponents.item(display.getItem(this.connecting.get())).get());
     }
 
     @Override
-    public boolean mousePressed(int mouseX, int mouseY, int button, boolean hasBeenHandled){
-        if(!hasBeenHandled && mouseX >= this.x && mouseX < this.x + this.width && mouseY >= this.y && mouseY < this.y + this.height){
-            ChiselingEntry entry = this.entry.get();
-            if(entry != null)
-                this.onClick.run();
-            return true;
+    public void render(PoseStack poseStack, int mouseX, int mouseY){
+        DisplayEntry display = this.entry.get();
+
+        boolean hasEntry = display != null;
+        boolean selected = hasEntry && this.selectedEntry.get() == display;
+        boolean hasCorrectItem = hasEntry && display.hasItem(this.connecting.get());
+
+        ScreenUtils.bindTexture(TEXTURE);
+        ScreenUtils.drawTexture(poseStack, this.x, this.y, this.width, this.height, 0, (selected ? 1 : hasEntry ? hasCorrectItem ? this.isFocused() ? 2 : 0 : this.isFocused() ? 4 : 3 : 0) / 5f, 1, 1 / 5f);
+    }
+
+    @Override
+    public void renderForeground(PoseStack poseStack, int mouseX, int mouseY){
+        DisplayEntry display = this.entry.get();
+        if(display != null){
+            Item item = display.getItem(this.connecting.get());
+            ScreenItemRender.drawItem(item, this.x + this.width / 2d, this.y + this.height / 2d, (this.width - 2) * 1.416, 0, 0, false);
         }
-        return super.mousePressed(mouseX, mouseY, button, hasBeenHandled);
     }
 }
