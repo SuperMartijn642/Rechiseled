85d760d37282807b86cece2c07bb5d4d15c90d50
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:01:59 +0100

    Check for optional flag when parsing chiseling entries

--- src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeDatapackPlugin.java
+++ src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeDatapackPlugin.java
@@ -100,8 +100,11 @@ public class ChiselingRecipeDatapackPlugin implements IdentifiableResourceReload
                     if(json.has("entries")){
                         if(!json.get("entries").isJsonArray())
                             throw new JsonParseException("Property 'entries' must be an array!");
-                        for(JsonElement element : json.getAsJsonArray("entries"))
-                            entries.add(ChiselingEntryImpl.fromJson(element));
+                        for(JsonElement element : json.getAsJsonArray("entries")){
+                            ChiselingEntry entry = ChiselingEntryImpl.fromJson(element);
+                            if(entry != null)
+                                entries.add(entry);
+                        }
                     }
                 }catch(JsonParseException e){
                     Rechiseled.LOGGER.error("Invalid chiseling recipe '{}' from '{}':\n   {}", recipeLocation, resource.sourcePackId(), e.getMessage(), e);
