4ddbe0a3066e81fc4f8ab9d57387fc983f3a5f8d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:08:03 +0100

    Rework chisel screen with search, filters, scrolling, slabs & stairs support

--- src/main/java/com/supermartijn642/rechiseled/screen/ChiselAllWidget.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/ChiselAllWidget.java
@@ -6,7 +6,7 @@ import com.supermartijn642.core.gui.widget.WidgetRenderContext;
 import com.supermartijn642.core.gui.widget.premade.AbstractButtonWidget;
 import com.supermartijn642.core.util.Holder;
 import com.supermartijn642.rechiseled.Rechiseled;
-import com.supermartijn642.rechiseled.api.chiseling.ChiselingEntry;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
@@ -21,11 +21,17 @@ public class ChiselAllWidget extends AbstractButtonWidget {
     public static final ResourceLocation GREY_BUTTONS = Rechiseled.identifier("screen/grey_buttons");
     public static final ResourceLocation CHISEL_TEXTURE = Rechiseled.identifier("item/chisel");
 
-    private final Supplier<ChiselingEntry> currentEntry;
+    private final Supplier<DisplayEntry> currentEntry;
+    public int chiselableItems = 0;
 
-    public ChiselAllWidget(int x, int y, int width, int height, Supplier<ChiselingEntry> currentEntry, Runnable onPress){
+    public ChiselAllWidget(int x, int y, int width, int height, Supplier<DisplayEntry> currentEntry, Runnable onPress){
         super(x, y, width, height, onPress);
         this.currentEntry = currentEntry;
+    }
+
+    @Override
+    protected boolean isClickable(){
+        return this.currentEntry.get() != null;
     }
 
     @Override
@@ -37,17 +43,17 @@ public class ChiselAllWidget extends AbstractButtonWidget {
 
     @Override
     protected void getTooltips(Consumer<Component> tooltips){
-        if(this.currentEntry.get() != null)
-            tooltips.accept(TextComponents.translation("rechiseled.chiseling.chisel_all").get());
+        if(this.currentEntry.get() != null){
+            tooltips.accept(TextComponents.translation("rechiseled.chiseling.chisel_all").bold().get());
+            tooltips.accept(TextComponents.translation("rechiseled.chiseling.chisel_all.shift", TextComponents.translation("key.keyboard.left.shift").color(ChatFormatting.GOLD).get()).get());
+            tooltips.accept(TextComponents.translation("rechiseled.chiseling.chisel_all.items", TextComponents.number(this.chiselableItems).get()).italic().color(ChatFormatting.GRAY).get());
+        }
     }
 
     @Override
     public void render(WidgetRenderContext context, GuiGraphicsHelper graphics, int mouseX, int mouseY){
-        ChiselingEntry currentEntry = this.currentEntry.get();
-        boolean hasEntry = currentEntry != null;
-
+        boolean hasEntry = this.currentEntry.get() != null;
         graphics.submitSprite(GREY_BUTTONS, this.x, this.y, this.width, this.height, p -> p.uv(0, (hasEntry ? this.isFocused() ? 2 : 0 : 1) / 3f, 1, 1 / 3f));
-
-        graphics.submitSprite(CHISEL_TEXTURE, this.x + 1, this.y + 2, this.width - 2, this.height - 4);
+        graphics.submitSprite(CHISEL_TEXTURE, this.x + 1, this.y + 2, this.width - 2, this.height - 4, p -> p.color(hasEntry ? 255 : 50, hasEntry ? 255 : 50, hasEntry ? 255 : 50));
     }
 }
