fb78bdfec7c6a42a2207d681f9bacaf0c31718cd
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:08:03 +0100

    Rework chisel screen with search, filters, scrolling, slabs & stairs support

--- src/main/java/com/supermartijn642/rechiseled/screen/ToggleRotationButton.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/ToggleRotationButton.java
@@ -7,6 +7,8 @@ import com.supermartijn642.rechiseled.Rechiseled;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.function.BooleanSupplier;
+
 /**
  * Created 20/04/2023 by SuperMartijn642
  */
@@ -14,15 +16,23 @@ public class ToggleRotationButton extends AbstractButtonWidget {
 
     private static final ResourceLocation TEXTURE = Rechiseled.identifier("textures/screen/rotation_icon.png");
 
-    public static boolean rotate = true;
+    private final BooleanSupplier rotating;
+    private final BooleanSupplier hasItem;
 
-    public ToggleRotationButton(int x, int y, int width, int height){
-        super(x, y, width, height, () -> rotate = !rotate);
+    public ToggleRotationButton(int x, int y, int width, int height, BooleanSupplier rotating, Runnable onPress, BooleanSupplier hasItem){
+        super(x, y, width, height, onPress);
+        this.rotating = rotating;
+        this.hasItem = hasItem;
+    }
+
+    @Override
+    protected boolean isClickable(){
+        return this.hasItem.getAsBoolean();
     }
 
     @Override
     public void render(WidgetRenderContext context, int mouseX, int mouseY){
-        ScreenUtils.drawTexture(TEXTURE, context.poseStack(), this.x, this.y, this.width, this.height, rotate ? 0 : 0.5f, this.isFocused() ? 0.5f : 0, 0.5f, 0.5f);
+        ScreenUtils.drawTexture(TEXTURE, context.poseStack(), this.x, this.y, this.width, this.height, this.rotating.getAsBoolean() ? 0 : 0.5f, this.isFocused() && this.hasItem.getAsBoolean() ? 0.5f : 0, 0.5f, 0.5f);
         super.render(context, mouseX, mouseY);
     }
 
