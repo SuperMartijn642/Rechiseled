527d3c7582fde5c5b168a9a578442ca01cdf7a25
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:11:47 +0100

    Rename `ScreenItemRender` to `ScreenItemRenderer`

--- src/main/java/com/supermartijn642/rechiseled/screen/preview/ScreenItemRenderer.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/preview/ScreenItemRenderer.java
@@ -15,7 +15,7 @@ import org.joml.Quaternionf;
 /**
  * Created 22/01/2022 by SuperMartijn642
  */
-public class ScreenItemRender {
+public class ScreenItemRenderer {
 
     /**
      * Renders a given item as a 3d model
