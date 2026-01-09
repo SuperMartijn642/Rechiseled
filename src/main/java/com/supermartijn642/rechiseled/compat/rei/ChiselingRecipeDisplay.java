85edc70712fc1b6017008a24d0c169438213e748
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-10 00:39:39 +0100

    Move conversion factor from chiseling shape to chiseling entry

--- src/main/java/com/supermartijn642/rechiseled/compat/rei/ChiselingRecipeDisplay.java
+++ src/main/java/com/supermartijn642/rechiseled/compat/rei/ChiselingRecipeDisplay.java
@@ -35,15 +35,15 @@ public class ChiselingRecipeDisplay implements Display {
             List<ItemStack> output = new ArrayList<>();
             if(entry.hasRegularItem(ChiselingBlockShape.BLOCK)){
                 //noinspection DataFlowIssue
-                inputs.add(new ItemStack(entry.getRegularItem(ChiselingBlockShape.BLOCK)));
+                inputs.add(new ItemStack(entry.getRegularItem(ChiselingBlockShape.BLOCK).item()));
                 //noinspection DataFlowIssue
-                output.add(new ItemStack(entry.getRegularItem(ChiselingBlockShape.BLOCK)));
+                output.add(new ItemStack(entry.getRegularItem(ChiselingBlockShape.BLOCK).item()));
             }
             if(entry.hasConnectingItem(ChiselingBlockShape.BLOCK)){
                 //noinspection DataFlowIssue
-                inputs.add(new ItemStack(entry.getConnectingItem(ChiselingBlockShape.BLOCK)));
+                inputs.add(new ItemStack(entry.getConnectingItem(ChiselingBlockShape.BLOCK).item()));
                 //noinspection DataFlowIssue
-                output.add(new ItemStack(entry.getConnectingItem(ChiselingBlockShape.BLOCK)));
+                output.add(new ItemStack(entry.getConnectingItem(ChiselingBlockShape.BLOCK).item()));
             }
             outputs.add(EntryIngredients.ofItemStacks(output));
         }
