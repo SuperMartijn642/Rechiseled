730f4fc4ce8fe3c94c946d7a42c82d3b0676da7f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:08:03 +0100

    Rework chisel screen with search, filters, scrolling, slabs & stairs support

--- src/main/java/com/supermartijn642/rechiseled/Rechiseled.java
+++ src/main/java/com/supermartijn642/rechiseled/Rechiseled.java
@@ -17,7 +17,6 @@ import com.supermartijn642.rechiseled.data.RechiseledRecipeGenerator;
 import com.supermartijn642.rechiseled.data.RechiseledTextureProvider;
 import com.supermartijn642.rechiseled.packet.PacketChiselAll;
 import com.supermartijn642.rechiseled.packet.PacketSelectEntry;
-import com.supermartijn642.rechiseled.packet.PacketToggleConnecting;
 import com.supermartijn642.rechiseled.screen.ChiselContainer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
@@ -68,7 +67,6 @@ public class Rechiseled implements ModInitializer {
     @Override
     public void onInitialize(){
         CHANNEL.registerMessage(PacketSelectEntry.class, PacketSelectEntry::new, PacketDirection.CLIENT_TO_SERVER, true);
-        CHANNEL.registerMessage(PacketToggleConnecting.class, PacketToggleConnecting::new, PacketDirection.CLIENT_TO_SERVER, true);
         CHANNEL.registerMessage(PacketChiselAll.class, PacketChiselAll::new, PacketDirection.CLIENT_TO_SERVER, true);
         CHANNEL.registerMessage(PacketUpdateChiselingRecipes.class, PacketUpdateChiselingRecipes::new, PacketDirection.SERVER_TO_CLIENT, true);
 
