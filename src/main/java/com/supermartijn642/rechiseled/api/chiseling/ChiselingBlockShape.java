12a7bc7daff8c174e62db769a28b70f2debaa1da
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-09 22:08:03 +0100

    Rework chisel screen with search, filters, scrolling, slabs & stairs support

--- src/main/java/com/supermartijn642/rechiseled/api/chiseling/ChiselingBlockShape.java
+++ src/main/java/com/supermartijn642/rechiseled/api/chiseling/ChiselingBlockShape.java
@@ -8,17 +8,23 @@ import net.minecraft.network.chat.Component;
  */
 public enum ChiselingBlockShape {
 
-    BLOCK("rechiseled.chiseling.shape.block"),
-    STAIRS("rechiseled.chiseling.shape.stairs"),
-    SLAB("rechiseled.chiseling.shape.slab"); // TODO generate translations
+    BLOCK("rechiseled.chiseling.shape.block", 1),
+    STAIRS("rechiseled.chiseling.shape.stairs", 1),
+    SLAB("rechiseled.chiseling.shape.slab", 2);
 
     private final Component name;
+    private final int conversionFactor;
 
-    ChiselingBlockShape(String translationKey){
+    ChiselingBlockShape(String translationKey, int conversionFactor){
         this.name = TextComponents.translation(translationKey).get();
+        this.conversionFactor = conversionFactor;
     }
 
     public Component translation(){
         return this.name;
+    }
+
+    public int conversionFactor(){
+        return this.conversionFactor;
     }
 }
