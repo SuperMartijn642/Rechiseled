51c7aaf69ec800390a33019e93f2714ce2e2b349
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-10 01:01:55 +0100

    Fix scrolling resetting when changing display entries

--- src/main/java/com/supermartijn642/rechiseled/screen/BaseChiselingContainerScreen.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/BaseChiselingContainerScreen.java
@@ -232,7 +232,6 @@ public class BaseChiselingContainerScreen<T extends BaseChiselingContainer> exte
         this.searchField.setTextSuppressed("");
         searchText = "";
         formattedSearchText = "";
-        this.setScrollOffset(0);
         this.searchField.setActive(this.recipe != null);
         this.filterOptionsWidget.setActive(this.recipe != null);
 
@@ -260,7 +259,9 @@ public class BaseChiselingContainerScreen<T extends BaseChiselingContainer> exte
             }
         }
         this.updateDisplayEntries();
-        if(matchingDisplay != null)
+        if(matchingDisplay == null)
+            this.setScrollOffset(0);
+        else
             this.selectDisplayEntry(matchingDisplay);
     }
 
