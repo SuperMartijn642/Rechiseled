36687e17e0c409e54f1dd074d34260585b438821
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-11 23:56:38 +0100

    Allow generating stairs and slabs through `RechiseledBlockBuilder`

--- src/main/java/com/supermartijn642/rechiseled/api/blocks/RechiseledBlockBuilder.java
+++ src/main/java/com/supermartijn642/rechiseled/api/blocks/RechiseledBlockBuilder.java
@@ -1,9 +1,7 @@
 package com.supermartijn642.rechiseled.api.blocks;
 
-import com.supermartijn642.core.block.BlockProperties;
 import com.supermartijn642.rechiseled.api.registration.RechiseledRegistration;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
 
 import java.util.function.Consumer;
@@ -15,12 +13,7 @@ import java.util.function.Supplier;
  * <p>
  * Created 26/04/2023 by SuperMartijn642
  */
-public interface RechiseledBlockBuilder {
-
-    /**
-     * Sets the properties for the constructed blocks
-     */
-    RechiseledBlockBuilder properties(BlockProperties properties);
+public interface RechiseledBlockBuilder extends RechiseledCommonBlockBuilder<RechiseledBlockBuilder> {
 
     /**
      * Copies the properties from the given block
@@ -30,61 +23,52 @@ public interface RechiseledBlockBuilder {
     RechiseledBlockBuilder copyProperties(Supplier<Block> block);
 
     /**
-     * Allows configuration of block properties
-     */
-    RechiseledBlockBuilder properties(Consumer<BlockProperties> configurer);
-
-    /**
-     * Sets the item groups the constructed blocks will be added to
-     */
-    RechiseledBlockBuilder itemGroups(CreativeModeTab group, CreativeModeTab... groups);
-
-    /**
      * Sets the type of block to be constructed
      */
     RechiseledBlockBuilder specification(BlockSpecification specification);
 
     /**
-     * Indicates that this block does not have a non-connecting variant
-     */
-    RechiseledBlockBuilder noRegularVariant();
-
-    /**
-     * Indicates that this block does not have a connecting variant
-     */
-    RechiseledBlockBuilder noConnectingVariant();
-
-    /**
      * Sets the regular variant for this block to the given block.
      * This is only relevant for the chiseling recipes.
      */
-    RechiseledBlockBuilder regularVariant(Supplier<Block> blockSupplier);
+    RechiseledBlockBuilder regularVariant(Supplier<Block> block, Supplier<Block> stairs, Supplier<Block> slab);
 
     /**
-     * Sets the connecting variant for this block to the given block.
+     * Sets the connecting variant for this block to the given block, stairs, and slab.
      * This is only relevant for the chiseling recipes.
      */
-    RechiseledBlockBuilder connectingVariant(Supplier<Block> blockSupplier);
+    RechiseledBlockBuilder connectingVariant(Supplier<Block> block, Supplier<Block> stairs, Supplier<Block> slab);
+
+    /**
+     * Creates stairs for this block.<br>
+     * Stairs will inherit properties from this block unless overwritten on their builder.
+     * @see #withStairs(Consumer) with stair configuration
+     */
+    RechiseledBlockBuilder withStairs();
+
+    /**
+     * Creates stairs for this block configure through the given consumer.<br>
+     * Stairs will inherit properties from this block unless overwritten on their builder.
+     */
+    RechiseledBlockBuilder withStairs(Consumer<RechiseledStairsBuilder> builder);
+
+    /**
+     * Creates slabs for this block configure through the returned builder.<br>
+     * Slabs will inherit properties from this block unless overwritten on their builder.
+     * @see #withSlabs(Consumer) with slab configuration
+     */
+    RechiseledBlockBuilder withSlabs();
+
+    /**
+     * Creates slabs for this block configure through the given consumer.<br>
+     * Slabs will inherit properties from this block unless overwritten on their builder.
+     */
+    RechiseledBlockBuilder withSlabs(Consumer<RechiseledSlabBuilder> builder);
 
     /**
      * Sets the chiseling recipe which this block should be added to.
      */
     RechiseledBlockBuilder recipe(Identifier location);
-
-    /**
-     * Adds the constructed block to the given tag.
-     */
-    RechiseledBlockBuilder blockTag(String namespace, String identifier);
-
-    /**
-     * Copies the mining tags from the given block.
-     */
-    RechiseledBlockBuilder miningTagsFrom(Supplier<Block> blockSupplier);
-
-    /**
-     * Sets the translation for the constructed block.
-     */
-    RechiseledBlockBuilder translation(String translation);
 
     /**
      * Sets a different model type to be generated.
