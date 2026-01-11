c9f90e9f514997767b5fe2b9f65af5baf981683c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-11 23:56:38 +0100

    Allow generating stairs and slabs through `RechiseledBlockBuilder`

---
+++ src/main/java/com/supermartijn642/rechiseled/blocks/RechiseledGlassSlabBlock.java
@@ -0,0 +1,39 @@
+package com.supermartijn642.rechiseled.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+/**
+ * Created 11/01/2026 by SuperMartijn642
+ */
+public class RechiseledGlassSlabBlock extends RechiseledSlabBlock {
+
+    public RechiseledGlassSlabBlock(boolean connecting, Properties properties){
+        super(connecting, properties);
+    }
+
+    @Override
+    public boolean skipRendering(BlockState state, BlockState otherState, Direction side){
+        return otherState.is(this) || super.skipRendering(state, otherState, side); // TODO
+    }
+
+    @Override
+    public VoxelShape getVisualShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context){
+        return Shapes.empty();
+    }
+
+    @Override
+    public float getShadeBrightness(BlockState state, BlockGetter level, BlockPos pos){
+        return 1;
+    }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter level, BlockPos pos){
+        return true;
+    }
+}
