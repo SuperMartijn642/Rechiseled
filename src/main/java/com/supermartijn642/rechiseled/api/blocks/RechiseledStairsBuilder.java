a5fbea3764de7aea9bf55209b87f5b710ef449ac
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-11 23:56:38 +0100

    Allow generating stairs and slabs through `RechiseledBlockBuilder`

---
+++ src/main/java/com/supermartijn642/rechiseled/api/blocks/RechiseledStairsBuilder.java
@@ -0,0 +1,11 @@
+package com.supermartijn642.rechiseled.api.blocks;
+
+/**
+ * Used to configure and build new slabs from blocks for use with Rechiseled.
+ * Obtained through {@link RechiseledBlockBuilder#withStairs()}.
+ * <p>
+ * Created 11/01/2026 by SuperMartijn642
+ * @see RechiseledBlockBuilder
+ */
+public interface RechiseledStairsBuilder extends RechiseledCommonBlockBuilder<RechiseledStairsBuilder> {
+}
