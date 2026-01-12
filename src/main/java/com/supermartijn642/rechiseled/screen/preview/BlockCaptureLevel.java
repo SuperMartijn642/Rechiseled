bdfdeddbdbe5c45092000303fbba9f62d3281af7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 18:28:36 +0100

    Improve entry preview lighting

--- src/main/java/com/supermartijn642/rechiseled/screen/preview/BlockCaptureLevel.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/preview/BlockCaptureLevel.java
@@ -56,7 +56,7 @@ public class BlockCaptureLevel implements LevelReader {
 
     @Override
     public float getShade(Direction side, boolean bl){
-        return ClientUtils.getWorld().getShade(side, bl);
+        return 1;
     }
 
     @Override
@@ -66,7 +66,7 @@ public class BlockCaptureLevel implements LevelReader {
 
     @Override
     public LevelLightEngine getLightEngine(){
-        return ClientUtils.getWorld().getLightEngine();
+        return null;
     }
 
     @Override
