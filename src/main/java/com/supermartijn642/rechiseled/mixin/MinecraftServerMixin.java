9fd678ee59443539c92cdcd3dd85eddca1108386
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 20:44:07 +0100

    Improve chiseling recipe sync timing

--- src/main/java/com/supermartijn642/rechiseled/mixin/MinecraftServerMixin.java
+++ src/main/java/com/supermartijn642/rechiseled/mixin/MinecraftServerMixin.java
@@ -6,23 +6,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Collection;
 
 /**
  * Created 07/01/2026 by SuperMartijn642
  */
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
-
-    @Inject(
-        method = "reloadResources",
-        at = @At("TAIL")
-    )
-    private void reloadResources(Collection<String> collection, CallbackInfoReturnable<?> ci){
-        ChiselingRecipeManagerImpl.loadRecipes();
-    }
 
     @Inject(
         method = "stopServer",
