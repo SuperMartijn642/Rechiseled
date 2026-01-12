d7f24e9f72d2a5bc34cb3b2d25a62720efbdc3d3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 18:28:36 +0100

    Improve entry preview lighting

--- src/main/java/com/supermartijn642/rechiseled/screen/preview/ScreenBlockRenderer.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/preview/ScreenBlockRenderer.java
@@ -3,25 +3,22 @@ package com.supermartijn642.rechiseled.screen.preview;
 import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Quaternion;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.render.RenderUtils;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.block.model.BakedQuad;
-import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.client.ChunkRenderTypeSet;
+import net.minecraftforge.client.RenderTypeHelper;
 import net.minecraftforge.client.model.data.ModelData;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,10 +27,10 @@ import java.util.Map;
 public class ScreenBlockRenderer {
 
     private static final PoseStack POSE_STACK = new PoseStack();
-    private static final RandomSource RANDOM = RandomSource.create();
+    private static final RandomSource RANDOM_SOURCE = RandomSource.create();
     private static BlockCaptureLevel fakeLevel;
 
-    public static void drawBlock(BlockCapture capture, double x, double y, double scale, float yaw, float pitch, boolean doShading){
+    public static void drawBlock(BlockCapture capture, double x, double y, double scale, float yaw, float pitch, boolean flatShading){
         AABB bounds = capture.getBounds();
         double span = Math.sqrt(bounds.getXsize() * bounds.getXsize() + bounds.getYsize() * bounds.getYsize() + bounds.getZsize() * bounds.getZsize());
         scale /= span;
@@ -51,20 +48,18 @@ public class ScreenBlockRenderer {
         POSE_STACK.scale((float)scale, (float)scale, (float)scale);
         POSE_STACK.mulPose(new Quaternion(pitch, yaw, 0, true));
 
-        if(doShading)
-            Lighting.setupFor3DItems();
-
-
-        MultiBufferSource.BufferSource renderTypeBuffer = RenderUtils.getMainBufferSource();
+        MultiBufferSource.BufferSource bufferSource = RenderUtils.getMainBufferSource();
         for(Map.Entry<BlockPos,BlockState> entry : capture.getBlocks())
-            renderBlock(entry.getKey(), entry.getValue(), POSE_STACK, renderTypeBuffer);
-        renderTypeBuffer.endBatch();
-
-        RenderSystem.enableDepthTest();
-        if(doShading)
-            Lighting.setupForFlatItems();
+            renderBlock(fakeLevel, entry.getKey(), entry.getValue(), POSE_STACK, bufferSource);
 
         POSE_STACK.popPose();
+
+        if(flatShading){
+            Lighting.setupForFlatItems();
+            bufferSource.endBatch();
+            Lighting.setupFor3DItems();
+        }else
+            bufferSource.endBatch();
 
         RenderSystem.getModelViewStack().popPose();
         RenderSystem.applyModelViewMatrix();
@@ -72,36 +67,21 @@ public class ScreenBlockRenderer {
         fakeLevel.setCapture(null);
     }
 
-    private static void renderBlock(BlockPos pos, BlockState state, PoseStack matrixStack, MultiBufferSource renderTypeBuffer){
-        matrixStack.pushPose();
-        matrixStack.translate(pos.getX() - 0.5, pos.getY() - 0.5, pos.getZ() - 0.5);
+    private static void renderBlock(BlockAndTintGetter level, BlockPos pos, BlockState state, PoseStack poseStack, MultiBufferSource bufferSource){
+        poseStack.pushPose();
+        poseStack.translate(pos.getX() - 0.5, pos.getY() - 0.5, pos.getZ() - 0.5);
 
-        BakedModel model = ClientUtils.getBlockRenderer().getBlockModel(state);
-        ModelData modelData = model.getModelData(fakeLevel, pos, state, ModelData.EMPTY);
-        RANDOM.setSeed(42);
-        ChunkRenderTypeSet renderTypes = model.getRenderTypes(state, RANDOM, modelData);
+        long seed = state.getSeed(pos);
+        BlockRenderDispatcher blockRenderer = ClientUtils.getBlockRenderer();
+        BakedModel model = blockRenderer.getBlockModel(state);
+        ModelData modelData = model.getModelData(level, pos, state, ModelData.EMPTY);
+        RANDOM_SOURCE.setSeed(seed);
+        ChunkRenderTypeSet renderTypes = model.getRenderTypes(state, RANDOM_SOURCE, modelData);
         for(RenderType renderType : renderTypes){
-            RenderType itemRenderType = renderType == RenderType.translucent() ? Sheets.translucentCullBlockSheet() : Sheets.cutoutBlockSheet();
-            renderModel(model, state, matrixStack, renderTypeBuffer.getBuffer(itemRenderType), modelData, renderType);
+            RenderType itemRenderType = RenderTypeHelper.getEntityRenderType(renderType, true);
+            blockRenderer.renderBatched(state, pos, level, poseStack, bufferSource.getBuffer(itemRenderType), true, RANDOM_SOURCE, modelData, renderType);
         }
 
-        matrixStack.popPose();
-    }
-
-    private static void renderModel(BakedModel modelIn, BlockState state, PoseStack matrixStackIn, VertexConsumer bufferIn, ModelData modelData, RenderType renderType){
-        for(Direction direction : Direction.values()){
-            RANDOM.setSeed(42);
-            renderQuads(matrixStackIn, bufferIn, modelIn.getQuads(state, direction, RANDOM, modelData, renderType));
-        }
-
-        RANDOM.setSeed(42);
-        renderQuads(matrixStackIn, bufferIn, modelIn.getQuads(state, null, RANDOM, modelData, renderType));
-    }
-
-    private static void renderQuads(PoseStack matrixStackIn, VertexConsumer bufferIn, List<BakedQuad> quadsIn){
-        PoseStack.Pose matrix = matrixStackIn.last();
-
-        for(BakedQuad bakedquad : quadsIn)
-            bufferIn.putBulkData(matrix, bakedquad, 1, 1, 1, 1, 15728880, OverlayTexture.NO_OVERLAY, false);
+        poseStack.popPose();
     }
 }
