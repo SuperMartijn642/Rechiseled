7efa24088043e7f7cb9709484c63b69cddba8dc7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 01:02:01 +0100

    Add `ChiselingConversionHelper`

--- src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeImpl.java
+++ src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeImpl.java
@@ -43,9 +43,8 @@ public class ChiselingRecipeImpl implements ChiselingRecipe {
     }
 
     @Override
-    public float getWorth(IItemProvider item){
-        ItemWithWorth worth = this.items.get(item.asItem());
-        return worth == null ? -1 : worth.worth();
+    public ItemWithWorth getWorth(IItemProvider item){
+        return this.items.get(item.asItem());
     }
 
     public Set<Item> getItems(){
