38b8ac5593ccf063865409d1baccd621eadf99ee
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 22:45:58 +0100

    Show recipe and owner of chiseling entries when advanced item info is enabled

--- src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeDatapackPlugin.java
+++ src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeDatapackPlugin.java
@@ -26,6 +26,7 @@ import java.util.stream.Collectors;
  */
 public class ChiselingRecipeDatapackPlugin implements PreparableReloadListener, ChiselingRecipePlugin {
 
+    public static final Identifier IDENTIFIER = Rechiseled.identifier("datapacks");
     public static final ChiselingRecipeDatapackPlugin INSTANCE = new ChiselingRecipeDatapackPlugin();
     private static final Gson GSON = new GsonBuilder().setStrictness(Strictness.LENIENT).create();
 
