f7ce0281e53522a5116a7d1a8524073382a39269
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 01:02:01 +0100

    Add `ChiselingConversionHelper`

--- src/main/java/com/supermartijn642/rechiseled/chiseling/plugin/MutableChiselingRecipeImpl.java
+++ src/main/java/com/supermartijn642/rechiseled/chiseling/plugin/MutableChiselingRecipeImpl.java
@@ -66,14 +66,14 @@ public class MutableChiselingRecipeImpl implements MutableChiselingRecipe {
     }
 
     @Override
-    public float getWorth(ItemLike item){
+    public ItemWithWorth getWorth(ItemLike item){
         ItemWithWorth largestWorth = null;
         for(ChiselingEntry entry : this.entries){
             ItemWithWorth worth = ((ChiselingEntryImpl)entry).items().get(item.asItem());
             if(entry.contains(item) && (largestWorth == null || largestWorth.worth() < worth.worth()))
                 largestWorth = worth;
         }
-        return largestWorth == null ? -1 : largestWorth.worth();
+        return largestWorth;
     }
 
     private class EntryBuilderImpl implements EntryBuilder {
