097a3d3e06fb453865a54c1a6efaab18a5c9ec77
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 20:41:43 +0100

    Fix block worth check in `ChiselItem#findChiselableBlocks`

--- src/main/java/com/supermartijn642/rechiseled/ChiselItem.java
+++ src/main/java/com/supermartijn642/rechiseled/ChiselItem.java
@@ -103,7 +103,6 @@ public class ChiselItem extends BaseItem {
         }
 
         // Get filter properties
-        ItemWithWorth filterWorth = filter.isEmpty() ? null : recipe.getWorth(filter.getItem());
         Block filterBlock = null;
         if(!filter.isEmpty()){
             item = filter.getItem();
@@ -143,7 +142,7 @@ public class ChiselItem extends BaseItem {
                     if(state.getBlock() == filterBlock)
                         continue;
                     ItemWithWorth worth = recipe.getWorth(state.getBlock());
-                    if(worth == null || (filterWorth != null && worth.worth() != filterWorth.worth()))
+                    if(worth == null)
                         continue;
                     // Check block is not hidden behind another block
                     pos.set(targetedPos.getX() + x + side.getStepX(), targetedPos.getY() + y + side.getStepY(), targetedPos.getZ() + z + side.getStepZ());
