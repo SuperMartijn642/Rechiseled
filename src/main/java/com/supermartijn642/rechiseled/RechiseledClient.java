34037f04d35902f02d11b1dc753bb43f6446bbba
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 07:06:41 +0100

    Add world left-click block chiseling

--- src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
+++ src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
@@ -12,6 +12,8 @@ import com.supermartijn642.rechiseled.screen.BaseChiselingContainerScreen;
 public class RechiseledClient {
 
     public static void register(){
+        ChiselItemHighlighter.registerListeners();
+
         ClientRegistrationHandler handler = ClientRegistrationHandler.get(Rechiseled.MODID);
         handler.registerContainerScreen(() -> Rechiseled.chisel_container, container -> WidgetContainerScreen.of(new BaseChiselingContainerScreen<>(TextComponents.item(Rechiseled.chisel).get()), container, false));
         handler.registerItemModelOverwrite(() -> Rechiseled.chisel, CustomRendererBakedModelWrapper::wrap);
