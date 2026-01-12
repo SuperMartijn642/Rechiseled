570efd86ad1b7a246546efc14f86ac738a07655a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 22:09:30 +0100

    Increase size of filter options marker

--- src/main/java/com/supermartijn642/rechiseled/screen/FilterOptionsWidget.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/FilterOptionsWidget.java
@@ -119,7 +119,7 @@ public class FilterOptionsWidget extends BaseWidget {
     public void render(WidgetRenderContext context, int mouseX, int mouseY){
         if(!this.showBlocks.getAsBoolean() || !this.showStairs.getAsBoolean() || !this.showSlabs.getAsBoolean() || !this.showNonConnecting.getAsBoolean()){
             ScreenUtils.bindTexture(MARKER);
-            ScreenUtils.drawTexture(context.poseStack(), this.x + 7, this.y + 1, 3, 3);
+            ScreenUtils.drawTexture(context.poseStack(), this.x + this.collapsedWidth - 4, this.y, 4, 4);
         }
     }
 
