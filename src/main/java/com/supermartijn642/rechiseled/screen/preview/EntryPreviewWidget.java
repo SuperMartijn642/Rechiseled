e5da5d568d5dcda366a2a509434deac28605a731
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-12 17:55:41 +0100

    Update states for surrounding blocks when rendering preview

--- src/main/java/com/supermartijn642/rechiseled/screen/preview/EntryPreviewWidget.java
+++ src/main/java/com/supermartijn642/rechiseled/screen/preview/EntryPreviewWidget.java
@@ -6,6 +6,8 @@ import com.supermartijn642.core.gui.CursorTypes;
 import com.supermartijn642.core.gui.widget.BaseWidget;
 import com.supermartijn642.rechiseled.screen.ToggleRotationButton;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
@@ -76,17 +78,24 @@ public class EntryPreviewWidget extends BaseWidget {
             if(item instanceof BlockItem){
                 // Render block
                 Block block = ((BlockItem)item).getBlock();
-                BlockCapture capture;
-                if(previewMode == PreviewMode.SINGLE)
-                    capture = new BlockCapture(block);
-                else if(previewMode == PreviewMode.ROW){
-                    capture = new BlockCapture(block);
-                    capture.putBlock(new BlockPos(-1, 0, 0), block);
-                    capture.putBlock(new BlockPos(1, 0, 0), block);
-                }else{
-                    capture = new BlockCapture();
-                    for(int i = 0; i < 9; i++)
-                        capture.putBlock(new BlockPos(i / 3 - 1, i % 3 - 1, 0), block);
+                BlockCapture capture = new BlockCapture();
+                int xRange = previewMode == PreviewMode.SINGLE ? 1 : 2;
+                int yRange = previewMode == PreviewMode.PANEL ? 2 : 1;
+                for(int x = -xRange; x <= xRange; x++){
+                    for(int y = -yRange; y <= yRange; y++){
+                        BlockPos pos = new BlockPos(x, y, 0);
+                        if(Math.abs(x) == xRange || Math.abs(y) == yRange)
+                            capture.putBlock(pos, Blocks.COBBLESTONE.defaultBlockState());
+                        else
+                            capture.putBlock(pos, block);
+                    }
+                }
+                capture.updateShapes();
+                for(int x = -xRange; x <= xRange; x++){
+                    for(int y = -yRange; y <= yRange; y++){
+                        if(Math.abs(x) == xRange || Math.abs(y) == yRange)
+                            capture.putBlock(new BlockPos(x, y, 0), (BlockState)null);
+                    }
                 }
                 ScreenBlockRenderer.drawBlock(capture, this.x + this.width / 2d, this.y + this.height / 2d, this.width, yaw, pitch, true);
             }else{
---
+++ src/main/java/com/supermartijn642/rechiseled/util/EmptyChunkSource.java
@@ -0,0 +1,42 @@
+package com.supermartijn642.rechiseled.util;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.chunk.AbstractChunkProvider;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.lighting.WorldLightManager;
+
+import javax.annotation.Nullable;
+import java.util.function.BooleanSupplier;
+
+/**
+ * Created 17/01/2026 by SuperMartijn642
+ */
+public class EmptyChunkSource extends AbstractChunkProvider {
+
+    public static final AbstractChunkProvider INSTANCE = new EmptyChunkSource();
+
+    @Override
+    public @Nullable Chunk getChunk(int i, int j, ChunkStatus chunkStatus, boolean bl){
+        return null;
+    }
+
+    @Override
+    public void tick(BooleanSupplier booleanSupplier){
+    }
+
+    @Override
+    public String gatherStats(){
+        return "";
+    }
+
+    @Override
+    public WorldLightManager getLightEngine(){
+        return null;
+    }
+
+    @Override
+    public IBlockReader getLevel(){
+        return null;
+    }
+}
