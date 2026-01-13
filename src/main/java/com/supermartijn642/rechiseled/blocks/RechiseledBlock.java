1d83bf866dbfd3e5e9740db6be8c8e209f6471c7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-13 21:14:06 +0100

    Add vanilla block property constructors #146

--- src/main/java/com/supermartijn642/rechiseled/blocks/RechiseledBlock.java
+++ src/main/java/com/supermartijn642/rechiseled/blocks/RechiseledBlock.java
@@ -2,6 +2,7 @@ package com.supermartijn642.rechiseled.blocks;
 
 import com.supermartijn642.core.block.BaseBlock;
 import com.supermartijn642.core.block.BlockProperties;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 /**
  * Created 22/12/2021 by SuperMartijn642
@@ -11,6 +12,11 @@ public class RechiseledBlock extends BaseBlock {
     private final boolean connecting;
 
     public RechiseledBlock(boolean connecting, BlockProperties properties){
+        super(false, properties);
+        this.connecting = connecting;
+    }
+
+    public RechiseledBlock(boolean connecting, BlockBehaviour.Properties properties){
         super(false, properties);
         this.connecting = connecting;
     }
