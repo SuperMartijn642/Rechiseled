2f6764156d2b58838e7253c3986d51b3214cde68
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-13 04:47:23 +0100

    Add separate models for glass stairs

--- src/main/java/com/supermartijn642/rechiseled/data/RechiseledBlockModelGenerator.java
+++ src/main/java/com/supermartijn642/rechiseled/data/RechiseledBlockModelGenerator.java
@@ -81,6 +81,169 @@ public class RechiseledBlockModelGenerator extends ModelGenerator {
                     .face(Direction.SOUTH, face -> face.texture("side").uv(8, 8, 16, 16))
                     .face(Direction.WEST, face -> face.texture("side").uv(0, 8, 8, 16))
             );
+
+        // Create glass stair models
+        this.model("block/glass_stairs")
+            .parent("minecraft", "block/stairs") // Use regular stairs as parent so we can inherit item transforms
+            .texture("particle", "#side")
+            .element(element ->
+                element.from(0, 0, 0).to(16, 8, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").cullface().uv(0, 0, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+            )
+            .element(element ->
+                element.from(0, 0, 0).to(8, 8, 16)
+                    .face(Direction.UP, face -> face.texture("top").uv(0, 0, 8, 16))
+            )
+            .element(element ->
+                element.from(8, 8, 0).to(16, 16, 16)
+                    .face(Direction.UP, face -> face.texture("top").cullface().uv(8, 0, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 0, 8, 8))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(8, 0, 16, 8))
+                    .face(Direction.WEST, face -> face.texture("side").uv(0, 0, 16, 8))
+            );
+        this.model("block/glass_inner_stairs")
+            .texture("particle", "#side")
+            .element(element ->
+                element.from(0, 0, 0).to(16, 8, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").cullface().uv(0, 0, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+            )
+            .element(element ->
+                element.from(0, 0, 0).to(8, 8, 8)
+                    .face(Direction.UP, face -> face.texture("top").uv(0, 0, 8, 8))
+            )
+            .element(element ->
+                element.from(8, 8, 0).to(16, 16, 16)
+                    .face(Direction.UP, face -> face.texture("top").cullface().uv(8, 0, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 0, 8, 8))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(8, 0, 16, 8))
+            )
+            .element(element ->
+                element.from(8, 8, 0).to(16, 16, 8)
+                    .face(Direction.WEST, face -> face.texture("side").uv(0, 0, 8, 8))
+            )
+            .element(element ->
+                element.from(0, 8, 8).to(8, 16, 16)
+                    .face(Direction.UP, face -> face.texture("top").cullface().uv(0, 8, 8, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").uv(8, 0, 16, 8))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(0, 0, 8, 8))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(8, 0, 16, 8))
+            );
+        this.model("block/glass_outer_stairs")
+            .texture("particle", "#side")
+            .element(element ->
+                element.from(0, 0, 0).to(16, 8, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").cullface().uv(0, 0, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+            )
+            .element(element ->
+                element.from(0, 0, 0).to(8, 8, 16)
+                    .face(Direction.UP, face -> face.texture("top").uv(0, 0, 8, 16))
+            )
+            .element(element ->
+                element.from(8, 0, 0).to(16, 8, 8)
+                    .face(Direction.UP, face -> face.texture("top").uv(8, 0, 16, 8))
+            )
+            .element(element ->
+                element.from(8, 8, 8).to(16, 16, 16)
+                    .face(Direction.UP, face -> face.texture("top").cullface().uv(8, 8, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").uv(0, 0, 8, 8))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 0, 8, 8))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(8, 0, 16, 8))
+                    .face(Direction.WEST, face -> face.texture("side").uv(8, 0, 16, 8))
+            );
+        this.model("block/glass_stairs_top")
+            .texture("particle", "#side")
+            .element(element ->
+                element.from(0, 8, 0).to(16, 16, 16)
+                    .face(Direction.UP, face -> face.texture("top").cullface())
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+            )
+            .element(element ->
+                element.from(0, 8, 0).to(8, 16, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").uv(0, 0, 8, 16))
+            )
+            .element(element ->
+                element.from(8, 0, 0).to(16, 8, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").cullface().uv(8, 0, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 8, 8, 16))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(8, 8, 16, 16))
+                    .face(Direction.WEST, face -> face.texture("side").uv(0, 8, 16, 16))
+            );
+        this.model("block/glass_inner_stairs_top")
+            .texture("particle", "#side")
+            .element(element ->
+                element.from(0, 8, 8).to(16, 16, 16)
+                    .face(Direction.UP, face -> face.texture("top").cullface())
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+            )
+            .element(element ->
+                element.from(0, 8, 0).to(8, 16, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").uv(0, 8, 8, 16))
+            )
+            .element(element ->
+                element.from(8, 0, 0).to(16, 8, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").cullface().uv(8, 0, 16, 16))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 8, 8, 16))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 8, 16, 16))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(8, 8, 16, 16))
+            )
+            .element(element ->
+                element.from(8, 0, 8).to(16, 8, 16)
+                    .face(Direction.WEST, face -> face.texture("side").uv(8, 8, 16, 16))
+            )
+            .element(element ->
+                element.from(0, 0, 0).to(8, 8, 8)
+                    .face(Direction.DOWN, face -> face.texture("bottom").cullface().uv(0, 0, 8, 8))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(8, 8, 16, 16))
+                    .face(Direction.SOUTH, face -> face.texture("side").uv(0, 8, 8, 16))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(0, 8, 8, 16))
+            );
+        this.model("block/glass_outer_stairs_top")
+            .texture("particle", "#side")
+            .element(element ->
+                element.from(0, 8, 0).to(16, 16, 16)
+                    .face(Direction.UP, face -> face.texture("top").cullface())
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.SOUTH, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+                    .face(Direction.WEST, face -> face.texture("side").cullface().uv(0, 0, 16, 8))
+            )
+            .element(element ->
+                element.from(0, 8, 0).to(8, 16, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").uv(0, 0, 8, 16))
+            )
+            .element(element ->
+                element.from(8, 8, 8).to(16, 16, 16)
+                    .face(Direction.DOWN, face -> face.texture("bottom").uv(0, 8, 8, 16))
+            )
+            .element(element ->
+                element.from(8, 0, 0).to(16, 8, 8)
+                    .face(Direction.DOWN, face -> face.texture("bottom").cullface().uv(8, 0, 16, 8))
+                    .face(Direction.NORTH, face -> face.texture("side").cullface().uv(0, 8, 8, 16))
+                    .face(Direction.EAST, face -> face.texture("side").cullface().uv(8, 8, 16, 16))
+                    .face(Direction.SOUTH, face -> face.texture("side").uv(8, 8, 16, 16))
+                    .face(Direction.WEST, face -> face.texture("side").uv(0, 8, 8, 16))
+            );
     }
 
     @Override
