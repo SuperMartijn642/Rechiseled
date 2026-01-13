2d5a85c2afbe1f57360b41f7d7dd461407eda3fb
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-13 20:38:33 +0100

    Update default plugin priority and sort equal priority plugins by identifier

--- src/main/java/com/supermartijn642/rechiseled/api/chiseling/plugin/ChiselingRecipePlugin.java
+++ src/main/java/com/supermartijn642/rechiseled/api/chiseling/plugin/ChiselingRecipePlugin.java
@@ -1,9 +1,20 @@
 package com.supermartijn642.rechiseled.api.chiseling.plugin;
 
+import com.supermartijn642.rechiseled.api.chiseling.ChiselingRecipeManager;
+
 /**
+ * Base plugin class for mods to add or modify chiseling recipes and listen chiseling recipe updates.
+ * <p>
+ * On Forge and NeoForge, plugins will automatically be registered if annotated with {@link RechiseledChiselingRecipePlugin}.<br>
+ * On Fabric, plugins should be listed as a 'rechiseled-chiseling-recipe-plugin' entry point in the <i>fabric.mod.json</i> properties.
+ * <p>
  * Created 07/01/2026 by SuperMartijn642
+ * @see RechiseledChiselingRecipePlugin annotation to register plugins automatically
+ * @see ChiselingRecipeManager for manually registering plugins
  */
 public interface ChiselingRecipePlugin {
+
+    int DEFAULT_PLUGIN_PRIORITY = 100;
 
     /**
      * Can be used to modify existing chiseling recipes or add new ones.
