8a06b9726bbf1c0016e9a1a19f9977cc3ace6d91
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-13 02:40:40 +0100

    Implement `#skipRendering` for transparent blocks, stairs, and slabs

--- src/main/java/com/supermartijn642/rechiseled/blocks/RechiseledGlassStairBlock.java
+++ src/main/java/com/supermartijn642/rechiseled/blocks/RechiseledGlassStairBlock.java
@@ -1,25 +1,54 @@
 package com.supermartijn642.rechiseled.blocks;
 
+import com.supermartijn642.core.util.Pair;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created 11/01/2026 by SuperMartijn642
  */
 public class RechiseledGlassStairBlock extends RechiseledStairBlock {
 
+    private Block block, slab;
+    private boolean haveBlockAndSlabBeenSet = false;
+    private volatile Map<Pair<BlockState,BlockState>,Boolean[]> shouldHideFaceCache = null;
+
     public RechiseledGlassStairBlock(boolean connecting, BlockState parent, Properties properties){
         super(connecting, parent, properties);
     }
 
+    public void setBlockAndSlab(Block block, Block slab){
+        if(this.haveBlockAndSlabBeenSet)
+            throw new IllegalStateException("Already set block and slab!");
+        this.haveBlockAndSlabBeenSet = true;
+        this.block = block;
+        this.slab = slab;
+    }
+
     @Override
     public boolean skipRendering(BlockState state, BlockState otherState, Direction side){
-        return otherState.is(this) || super.skipRendering(state, otherState, side); // TODO
+        if(!this.haveBlockAndSlabBeenSet)
+            throw new IllegalStateException("Block and slab have not been set!");
+        if(!otherState.is(this) && !otherState.is(this.block) && !otherState.is(this.slab))
+            return false;
+        // Create cache if absent
+        if(this.shouldHideFaceCache == null){
+            synchronized(this){
+                if(this.shouldHideFaceCache == null)
+                    this.shouldHideFaceCache = new HashMap<>();
+            }
+        }
+        // Get or calculate value
+        return RechiseledGlassBlock.calculateShouldHideFace(state, otherState, side, this.shouldHideFaceCache);
     }
 
     @Override
