39263745afce93f4a80e5dbac2d2bd55659c7bf7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-13 02:45:28 +0100

    Set `#noOcclusion` for glass blocks

--- src/main/java/com/supermartijn642/rechiseled/blocks/RechiseledGlassSlabBlock.java
+++ src/main/java/com/supermartijn642/rechiseled/blocks/RechiseledGlassSlabBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -23,7 +24,7 @@ public class RechiseledGlassSlabBlock extends RechiseledSlabBlock {
     private volatile Map<Pair<BlockState,BlockState>,Boolean[]> shouldHideFaceCache = null;
 
     public RechiseledGlassSlabBlock(boolean connecting, Properties properties){
-        super(connecting, properties);
+        super(connecting, properties.noOcclusion().isSuffocating(Blocks::never).isViewBlocking(Blocks::never));
     }
 
     public void setStairsAndSlab(Block block, Block stairs){
