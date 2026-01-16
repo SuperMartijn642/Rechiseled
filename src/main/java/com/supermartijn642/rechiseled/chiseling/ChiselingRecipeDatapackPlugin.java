40777c41fc57a8b3b1ee4070edf7e5b3689ebb53
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-16 14:47:16 +0100

    Remove file extension from chiseling recipe identifiers

--- src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeDatapackPlugin.java
+++ src/main/java/com/supermartijn642/rechiseled/chiseling/ChiselingRecipeDatapackPlugin.java
@@ -127,7 +127,11 @@ public class ChiselingRecipeDatapackPlugin implements IdentifiableResourceReload
             Rechiseled.LOGGER.error("Encountered an exception whilst trying to load chiseling recipe '{}'!", recipeLocation, e);
             return null;
         }
-        return new ChiselingRecipeProperties(recipeLocation, combinedEntries);
+        String recipeIdentifier = recipeLocation.getPath().substring("chiseling_recipes/".length(), recipeLocation.getPath().length() - ".json".length());
+        return new ChiselingRecipeProperties(
+            new ResourceLocation(recipeLocation.getNamespace(), recipeIdentifier),
+            combinedEntries
+        );
     }
 
     private record ChiselingRecipeProperties(ResourceLocation identifier, List<ChiselingEntry> entries) {
