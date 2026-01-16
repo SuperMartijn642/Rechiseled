ae7f2f2d5061c834661060492b89423f5c0057c3
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-16 22:23:20 +0100

    Remove legacy connecting model code

--- src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
+++ src/main/java/com/supermartijn642/rechiseled/RechiseledClient.java
@@ -1,27 +1,16 @@
 package com.supermartijn642.rechiseled;
 
 import com.supermartijn642.core.TextComponents;
-import com.supermartijn642.core.gui.BaseContainer;
 import com.supermartijn642.core.gui.WidgetContainerScreen;
-import com.supermartijn642.core.gui.widget.Widget;
 import com.supermartijn642.core.registry.ClientRegistrationHandler;
 import com.supermartijn642.core.render.CustomRendererBakedModelWrapper;
-import com.supermartijn642.rechiseled.model.RechiseledModelLoader;
 import com.supermartijn642.rechiseled.screen.BaseChiselingContainer;
 import com.supermartijn642.rechiseled.screen.BaseChiselingContainerScreen;
 import com.supermartijn642.rechiseled.screen.ChiselContainer;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 /**
  * Created 21/12/2021 by SuperMartijn642
  */
-@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RechiseledClient {
 
     public static void register(){
@@ -36,14 +25,5 @@ public class RechiseledClient {
         });
         handler.registerItemModelOverwrite(() -> Rechiseled.chisel, CustomRendererBakedModelWrapper::wrap);
         handler.registerCustomItemRenderer(() -> Rechiseled.chisel, ChiselItemRenderer::new);
-    }
-
-    /**
-     * TODO: remove in 1.2
-     */
-    @SubscribeEvent
-    @Deprecated
-    public static void onModelRegistry(ModelRegistryEvent e){
-        ModelLoaderRegistry.registerLoader(new ResourceLocation("rechiseled", "connecting_model"), new RechiseledModelLoader());
     }
 }
