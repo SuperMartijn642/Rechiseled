68e74e3ec88cdfc2c416215833cd0b5aa45a2e7f
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-01-24 04:07:19 +0100

    Make slab loot table drop 2 slabs for double slabs

--- src/main/java/com/supermartijn642/rechiseled/registration/data/RegistrationLootTableGenerator.java
+++ src/main/java/com/supermartijn642/rechiseled/registration/data/RegistrationLootTableGenerator.java
@@ -2,6 +2,10 @@ package com.supermartijn642.rechiseled.registration.data;
 
 import com.supermartijn642.core.generator.LootTableGenerator;
 import com.supermartijn642.core.generator.ResourceCache;
+import com.supermartijn642.core.loot_table.BlockStatePropertyLootCondition;
+import com.supermartijn642.core.util.Pair;
+import com.supermartijn642.rechiseled.blocks.impl.SlabBlock;
+import com.supermartijn642.rechiseled.blocks.impl.SlabType;
 import com.supermartijn642.rechiseled.registration.RechiseledRegistrationImpl;
 import net.minecraft.block.Block;
 
@@ -28,14 +32,14 @@ public class RegistrationLootTableGenerator extends LootTableGenerator {
                     if(builder.hasStairs() && builder.getStairs().hasRegularVariant())
                         this.addLootTable(builder.getStairs().getRegularBlock());
                     if(builder.hasSlabs() && builder.getSlabs().hasRegularVariant())
-                        this.addLootTable(builder.getSlabs().getRegularBlock());
+                        this.addSlabLootTable(builder.getSlabs().getRegularBlock());
                 }
                 if(builder.hasConnectingVariant()){
                     this.addLootTable(builder.getConnectingBlock());
                     if(builder.hasStairs() && builder.getStairs().hasConnectingVariant())
                         this.addLootTable(builder.getStairs().getConnectingBlock());
                     if(builder.hasSlabs() && builder.getSlabs().hasConnectingVariant())
-                        this.addLootTable(builder.getSlabs().getConnectingBlock());
+                        this.addSlabLootTable(builder.getSlabs().getConnectingBlock());
                 }
             }
         );
@@ -43,5 +47,22 @@ public class RegistrationLootTableGenerator extends LootTableGenerator {
 
     private void addLootTable(Block block){
         this.dropSelf(block);
+    }
+
+    private void addSlabLootTable(Block slab){
+        //noinspection SuspiciousNameCombination
+        this.lootTable(slab)
+            .pool(pool ->
+                pool.survivesExplosionCondition()
+                    .condition(new BlockStatePropertyLootCondition(slab, Pair.of(SlabBlock.TYPE, SlabType.BOTTOM), Pair.of(SlabBlock.TYPE, SlabType.TOP)))
+                    .survivesExplosionCondition()
+                    .itemEntry(slab)
+            )
+            .pool(pool ->
+                pool.survivesExplosionCondition()
+                    .condition(new BlockStatePropertyLootCondition(slab, Pair.of(SlabBlock.TYPE, SlabType.DOUBLE)))
+                    .survivesExplosionCondition()
+                    .itemEntry(slab, 2, 1)
+            );
     }
 }
